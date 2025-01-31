49f4fb63593e48ee6c86291c409c2b330b6bd12e
Oluwadara Oloye <daraoloye99@gmail.com>
2025-01-31 12:57:54 +0100

    feature: setup validation middleware

---
+++ middlewares/validators/authValidator.js
@@ -0,0 +1,40 @@
+const { body } = require('express-validator');
+
+const registerValidator = [
+  body("first_name")
+    .trim()
+    .notEmpty()
+    .withMessage("First name is required")
+    .isAlpha()
+    .withMessage("First name must contain only letters")
+    .isLength({ min: 2 })
+    .withMessage("First name must be at least 2 characters long"),
+
+  body("last_name")
+    .trim()
+    .notEmpty()
+    .withMessage("Last name is required")
+    .isAlpha()
+    .withMessage("Last name must contain only letters")
+    .isLength({ min: 2 })
+    .withMessage("Last name must be at least 2 characters long"),
+
+  body("email")
+    .trim()
+    .notEmpty()
+    .withMessage("Email is required")
+    .isEmail()
+    .withMessage("Invalid email format")
+    .normalizeEmail(),
+
+  body("password")
+    .notEmpty()
+    .withMessage("Password is required")
+    .isLength({ min: 6 })
+    .withMessage("Password must be at least 6 characters long"),
+]
+
+
+module.exports = {
+  registerValidator
+}
--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
+const authValidator = require("../middlewares/validators/authValidator")
+const validate = require("../middlewares/validators/validate");
 
 router.post('/', authController.login)
 
-router.post('/register', authController.register)
+router.post('/register',  authValidator.registerValidator, validate, authController.register)
 
 module.exports = router;
