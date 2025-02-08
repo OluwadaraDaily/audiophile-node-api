3218eed8430647b46e73628e94685f8395505c94
Oluwadara Oloye <daraoloye99@gmail.com>
2025-02-08 14:29:01 +0100

    feature: implement activation endpoint and validation

--- routes/auth.js
+++ routes/auth.js
@@ -6,6 +6,8 @@ const validate = require("../middlewares/validators/validate");
 
 router.post('/', authController.login)
 
-router.post('/register',  authValidator.registerValidator, validate, authController.register)
+router.post('/register', authValidator.registerValidator, validate, authController.register)
+
+router.get(`/activate`, authValidator.activateValidator, validate, authController.activate)
 
 module.exports = router;
