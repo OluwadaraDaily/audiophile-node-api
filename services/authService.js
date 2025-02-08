7de0d2d3a4210c4f1425a3412478a128d662c21e
Oluwadara Oloye <daraoloye99@gmail.com>
2025-02-08 21:01:53 +0100

    feature: implement activate service actions

--- services/authService.js
+++ services/authService.js
@@ -1,9 +1,9 @@
+require('dotenv').config({ path: "../.env" });
+
 const db = require("../config/db/db");
 const bcrypt = require('bcrypt');
 const userService = require("./userService");
-const {
-  randomBytes,
-} = require('node:crypto');
+const emailService = require("./mailService");
 
 const registerUser = async (userInfo) => {
   const { first_name, last_name, email, password } = userInfo;
@@ -13,11 +13,24 @@ const registerUser = async (userInfo) => {
   }
 
   const hashedPassword = await hashPassword(password);
-  const { token, hashedToken } = await generateToken();
+  const token = await generateToken();
 
   // Token will expire in 2 hours
   let tokenExpiryDate = new Date()
   tokenExpiryDate.setHours(tokenExpiryDate.getHours() + 2)
+
+  // Send user email
+  const emailOptions = {
+    to: email,
+    subject: 'Verify your account',
+    text: `Hello, Oluwadara. Your token for activation is ${token}`
+  }
+
+  console.log('HERE 1')
+
+  await emailService.sendEmail(emailOptions)
+
+  console.log('HERE 2')
   
   await db('users').insert({
     first_name,
@@ -25,7 +38,7 @@ const registerUser = async (userInfo) => {
     email,
     password:hashedPassword,
     is_activated: false,
-    token: hashedToken,
+    token,
     token_expiry: tokenExpiryDate
   })
 
@@ -44,14 +57,61 @@ async function comparePassword(password, hash) {
   return result;
 }
 
-const generateToken = async () => {
-  const token = randomBytes(32).toString('hex');
-  const saltRounds = 10;
-  const hashedToken = await bcrypt.hash(token, saltRounds);
-  return { token, hashedToken }
+const generateToken = async (length = 6) => {
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ123567890'
+  let token = ""
+  for (let i = 0; i < length; i++) {
+    token += chars[Math.floor(Math.random() * length)]
+  }
+  console.log('TOKEN =>', token)
+  return token
+}
+
+const verifyToken = async (token) => {
+  let query = db('users').whereNotNull('token')
+  
+  // Check if token exists
+  const userHasToken = await query.where({ token }).first()
+
+  if (!userHasToken) {
+    return {
+      message: `User with token (${token}) does not exist`
+    }  
+  }
+  
+  // Check if token has expired
+  const tokenHasExpired = await query.where('token_expiry', '<', db.raw("NOW()"))
+  
+  if (tokenHasExpired) {
+    return {
+      message: `Token has expired`
+    }
+  }
+
+  const userWithToken = await query
+    .where({ token })
+    .andWhere('token_expiry', '>', db.raw("NOW()"))
+    .first();
+  
+  // Update user
+  await db('users').where({ id: userWithToken.id }).update({
+    token_expiry: null,
+    is_activated: true
+  })
+
+  const user = await db('users').where({ id: userWithToken.id }).first();
+
+  const safeUser = {
+    first_name: user.first_name,
+    last_name: user.last_name,
+    email: user.email,
+    is_activated: user.is_activated
+  }
+  return {user: safeUser}
 }
 
 
 module.exports = {
-  registerUser
+  registerUser,
+  verifyToken
 }
