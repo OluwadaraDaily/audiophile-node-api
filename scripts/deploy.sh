252f16960783ebe85111d6dc14a1ba73545f4bb4
Oluwadara Oloye <daraoloye99@gmail.com>
2025-02-20 12:34:05 +0100

    chore: update deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,6 +9,9 @@ cd ~/audiophile-node-api
 echo "Checking out to main branch..."
 git checkout main
 
+# Set git config to allow for pulling
+git config --global pull.rebase false
+
 # # Check if package.json has changed
 # if git diff --name-only HEAD~1 | grep -q "package.json"; then
 #   echo "package.json has changed. Committing package-lock.json..."
